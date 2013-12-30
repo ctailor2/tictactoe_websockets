d2c6b0b2c429fba3354c3b80b390df946aa5ffa9
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-30 15:47:40 -0600

    Configure capybara to use selenium on chrome browser for javascript testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,4 +1,8 @@
 $LOAD_PATH << File.expand_path('../../..', __FILE__)
-require 'capybara/cucumber'
 require 'app'
+require 'capybara/cucumber'
 Capybara.app = App
+Capybara.javascript_driver = :selenium
+Capybara.register_driver :selenium do |app|
+	Capybara::Selenium::Driver.new(app, :browser => :chrome)
+end
