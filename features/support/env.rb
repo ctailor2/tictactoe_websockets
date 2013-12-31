1c20ae30f336dc5207f947a5819d5373e379242b
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2013-12-30 19:01:36 -0600

    Configure capybara to run puma server via config.ru file for testing

--- features/support/env.rb
+++ features/support/env.rb
@@ -1,7 +1,9 @@
-$LOAD_PATH << File.expand_path('../../..', __FILE__)
-require 'app'
 require 'capybara/cucumber'
-Capybara.app = App
+Capybara.app = Rack::Builder.parse_file(File.expand_path('../../../config.ru', __FILE__)).first
+Capybara.server do |app, port|
+	require 'rack/handler/puma'
+	Rack::Handler::Puma.run(app, :Port => port)
+end
 Capybara.javascript_driver = :selenium
 Capybara.register_driver :selenium do |app|
 	Capybara::Selenium::Driver.new(app, :browser => :chrome)
