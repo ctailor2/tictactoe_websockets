5082ae4a32424e27186c05c799f5113053448787
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2014-01-01 19:01:19 -0600

    New feature test to encapsulate 'start & join game' interactions

--- features/step_definitions/tictactoe_steps.rb
+++ features/step_definitions/tictactoe_steps.rb
@@ -1,25 +1,45 @@
 # Player starts game
 
-Given(/^I am not yet playing$/) do
+# Given(/^I am not yet playing$/) do
+# end
+
+# When(/^I start a game$/) do
+# 	visit "/"
+# end
+
+# Then(/^I should see "(.*?)"$/) do |message|
+# 	expect(page).to have_text(message)
+# end
+
+# # Player joins game
+
+# Given(/^a player has already started a game$/) do
+# 	session = Capybara::Session.new(:rack_test, App)
+# 	session.visit "/"
+# end
+
+# When(/^I join the game$/) do
+# 	visit "/"
+# end
+
+# Then(/^not "(.*?)"$/) do |message|
+# 	expect(page).not_to have_text(message)
+# end
+
+Given(/^the following users exist:$/) do |table|
+  # table is a Cucumber::Ast::Table
 end
 
-When(/^I start a game$/) do
+When(/^I am in (User.{1})'s browser$/) do |name|
+	Capybara.session_name = name
+end
+
+When(/^I enter the lobby$/) do
 	visit "/"
 end
 
 Then(/^I should see "(.*?)"$/) do |message|
 	expect(page).to have_text(message)
-end
-
-# Player joins game
-
-Given(/^a player has already started a game$/) do
-	session = Capybara::Session.new(:rack_test, App)
-	session.visit "/"
-end
-
-When(/^I join the game$/) do
-	visit "/"
 end
 
 Then(/^not "(.*?)"$/) do |message|
---
+++ features/users_enter_lobby.feature
@@ -0,0 +1,19 @@
+@javascript
+Feature: users enter lobby
+
+	The lobby is where users meet to play tictactoe against each other. Once exactly two users have entered the lobby, a new game starts with the first user's turn.
+
+	Scenario: two users enter the lobby
+		Given the following users exist:
+			| Name  |
+			| UserA |
+			| UserB |
+		When I am in UserA's browser
+		And I enter the lobby
+		Then I should see "Welcome to TicTacToe!"
+		And I should see "Waiting for Challenger"
+
+		When I am in UserB's browser
+		And I enter the lobby
+		Then I should see "Welcome to TicTacToe!"
+		But not "Waiting for Challenger"
