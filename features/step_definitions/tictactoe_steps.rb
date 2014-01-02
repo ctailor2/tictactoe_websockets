f887e8f4f308d2d4d5f3b8f8fe5abb364da2552f
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2014-01-01 19:04:46 -0600

    Clean up previous feature implementation

--- features/step_definitions/tictactoe_steps.rb
+++ features/step_definitions/tictactoe_steps.rb
@@ -1,30 +1,4 @@
-# Player starts game
-
-# Given(/^I am not yet playing$/) do
-# end
-
-# When(/^I start a game$/) do
-# 	visit "/"
-# end
-
-# Then(/^I should see "(.*?)"$/) do |message|
-# 	expect(page).to have_text(message)
-# end
-
-# # Player joins game
-
-# Given(/^a player has already started a game$/) do
-# 	session = Capybara::Session.new(:rack_test, App)
-# 	session.visit "/"
-# end
-
-# When(/^I join the game$/) do
-# 	visit "/"
-# end
-
-# Then(/^not "(.*?)"$/) do |message|
-# 	expect(page).not_to have_text(message)
-# end
+# Users enter lobby
 
 Given(/^the following users exist:$/) do |table|
   # table is a Cucumber::Ast::Table
