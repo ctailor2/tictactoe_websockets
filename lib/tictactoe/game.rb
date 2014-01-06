e1d5def26bdc3cd24c33b39876eac96a354bdb1d
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2014-01-06 17:38:36 -0600

    Game #initialize and #turn! methods implementation

--- lib/tictactoe/game.rb
+++ lib/tictactoe/game.rb
@@ -1,4 +1,14 @@
 class Game
+	attr_reader :players
+
 	def initialize(clients)
+		@players = clients
+		turn!
+	end
+
+	def turn!
+		players.first.send({ :turn_message => 'Your Turn' }.to_json)
+		players.last.send({ :turn_message => "Opponent's Turn" }.to_json)
+		players.rotate!
 	end
 end
