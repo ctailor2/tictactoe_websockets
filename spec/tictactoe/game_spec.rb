fe4085444ce33dc0b04868d16317d664f6d55049
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2014-01-06 17:38:01 -0600

    Game #initialize and #turn! methods tests

--- spec/tictactoe/game_spec.rb
+++ spec/tictactoe/game_spec.rb
@@ -1,1 +1,36 @@
 require 'spec_helper'
+
+describe Game do
+	let(:game) do
+		clients = []
+		2.times do |i|
+			clients << double("client#{i + 1}").as_null_object
+		end
+		Game.new(clients)
+	end
+
+	describe "#initialize" do
+		it "responds to #players" do
+			expect(game).to respond_to(:players)
+		end
+	end
+
+	describe "#turn!" do
+		it "sends the message 'Your Turn' to the first player" do
+			data = { :turn_message => 'Your Turn' }
+			expect(game.players.first).to receive(:send).with(data.to_json)
+			game.turn!
+		end
+
+		it "sends the message 'Opponent's Turn' to the second player" do
+			data = { :turn_message => "Opponent's Turn" }
+			expect(game.players.last).to receive(:send).with(data.to_json)
+			game.turn!
+		end
+
+		it "rotates the players" do
+			expect(game.players).to receive(:rotate!)
+			game.turn!
+		end
+	end
+end
