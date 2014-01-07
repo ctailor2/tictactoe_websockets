1bb2d90763dc664aee438c84e042589e4be90ad8
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2014-01-06 21:28:56 -0600

    Server #update_status method tests

--- spec/tictactoe/server_spec.rb
+++ spec/tictactoe/server_spec.rb
@@ -25,6 +25,11 @@ describe Server do
 	end
 
 	describe "#new_game" do
+		it "clears the client's status messages" do
+			expect(server).to receive(:update_status).with('', server.clients)
+			server.new_game
+		end
+
 		it "creates a new game with the server's clients" do
 			expect(Game).to receive(:new).with(server.clients)
 			server.new_game
@@ -32,6 +37,27 @@ describe Server do
 
 		it "sets the server's game attribute" do
 			expect{server.new_game}.to change{server.game}.from(nil).to(kind_of(Game))
+		end
+	end
+
+	describe "#update_status" do
+		context "when a single client is specified" do
+			it "sends the specified status message to the specified client(s)" do
+				client = server.clients.first
+				message = "Test message for single client"
+				expect(client).to receive(:send).with({:status => message}.to_json)
+				server.update_status(message, client)
+			end
+		end
+
+		context "when a collection of one or more clients is specified" do
+			it "sends the specified status message to the specified client(s)" do
+				clients = server.clients
+				message = "Test message for multiple clients"
+				expect(clients.first).to receive(:send).with({:status => message}.to_json)
+				expect(clients.last).to receive(:send).with({:status => message}.to_json)
+				server.update_status(message, clients)
+			end
 		end
 	end
 end
