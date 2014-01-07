453dcdce91d57d167e7999af6bc2746a3d316955
Chirag Tailor <chiragtailor@Chirags-MacBook-Pro.local>
2014-01-07 12:09:44 -0600

    Basic player places marker scenario

---
+++ features/player_places_marker.feature
@@ -0,0 +1,18 @@
+@javascript
+Feature: player places marker
+
+	As a player
+	I want to place my marker
+	So that I can complete my turn
+
+	Scenario: first player clicks unoccupied space on their turn
+		Given UserA & UserB are playing
+		And it is UserA's turn
+		When I am in UserA's browser
+		And I click on the middle space
+		Then I should see an 'X' in the middle space
+		And I should see "Opponent's Turn"
+
+		When I am in UserB's browser
+		Then I should see an 'X' in the middle space
+		And I should see "Your Turn"
--- features/step_definitions/tictactoe_steps.rb
+++ features/step_definitions/tictactoe_steps.rb
@@ -37,3 +37,21 @@ Then(/^a new game should have started$/) do
 	server = page.driver.app
 	expect(server.game).to be_an_instance_of(Game)
 end
+
+Given(/^UserA & UserB are playing$/) do
+	Capybara.using_session("UserA") { visit "/" }
+	Capybara.using_session("UserB") { visit "/" }
+end
+
+Given(/^it is UserA's turn$/) do
+end
+
+When(/^I click on the middle space$/) do
+	middle_space = page.find_by_id('5')
+	middle_space.click
+end
+
+Then(/^I should see an 'X' in the middle space$/) do
+	middle_space = page.find_by_id('5')
+	expect(middle_space).to have_text('X')
+end
