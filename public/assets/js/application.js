149a9f74ca046edfdf45bb682249f791c5dbda7c
Chirag Tailor <ctailor2@gmail.com>
2014-01-09 10:51:32 -0600

    Refactor message handler to use switch statement

--- public/assets/js/application.js
+++ public/assets/js/application.js
@@ -5,32 +5,31 @@ var ws = new WebSocket(uri);
 ws.onmessage = function(message) {
 	var data = JSON.parse(message.data)
 	console.log(data)
-	if (data.hasOwnProperty('status')){
-		$(".status").text(data.status);
-	}
-	else if (data.hasOwnProperty('turn_message')) {
-		var message = data.turn_message
-		$(".turn-message").text(message);
-		var clickHandler = function() {
-			var id = parseInt(this.id);
-			ws.send(JSON.stringify({ 'marker_message' : id }));
-		}
-		if (message === "Your Turn") {
-			$(".space").bind("click.myEvent", clickHandler);
-		}
-		else {
-			$(".space").unbind("click.myEvent");
-		}
-	}
-	else if (data.hasOwnProperty('display_message')) {
-		var message = data.display_message
-		if (message === 'show') {
-			$(".game-board").show('slow').css('display', 'inline-block');
-		}
-	}
-	else if (data.hasOwnProperty('marker_message')) {
-		var id = data.marker_message
-		$("#" + id).text("X");
+	var data_label = Object.keys(data)[0]
+	switch (data_label) {
+		case "status":
+			$(".status").text(data.status);
+		case "turn_message":
+			var message = data.turn_message
+			$(".turn-message").text(message);
+			var clickHandler = function() {
+				var id = parseInt(this.id);
+				ws.send(JSON.stringify({ 'marker_message' : id }));
+			}
+			if (message === "Your Turn") {
+				$(".space").bind("click.myEvent", clickHandler);
+			}
+			else {
+				$(".space").unbind("click.myEvent");
+			}
+		case "display_message":
+			var message = data.display_message;
+			if (message === 'show') {
+				$(".game-board").show('slow').css('display', 'inline-block');
+			}
+		case "marker_message":
+			var id = data.marker_message;
+			$("#" + id).text("X");
 	}
 }
 
