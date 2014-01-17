948f9dc1d6ab008f8d4e198576dbe23d498829ab
Chirag Tailor <ctailor2@gmail.com>
2014-01-17 15:21:37 -0600

    Implement status message for scenario where >2 users enter the lobby

--- middlewares/websocket_server.rb
+++ middlewares/websocket_server.rb
@@ -18,8 +18,14 @@ class Server
       ws.on :open do |event|
         p [:open, ws.object_id]
         clients << ws
-        new_game if new_game_req_met?
-        send_data(:status, 'Waiting for Challenger', ws) unless game
+        case num_clients
+        when 1
+          send_data(:status, 'Waiting for Challenger', ws)
+        when 2
+          new_game
+        else
+          send_data(:status, 'Game in Progress - Please try again later.', ws)
+        end
       end
 
       ws.on :message do |event|
